e2a1436e85857e8ead6ca5c9aeae2ace6c090c68
Shyam Kumar <imshyam2021@gmail.com>
2024-07-15 16:06:38 +0530

    hehe

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,57 @@
 import React from "react";
-import Navbar from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { FaBars } from "react-icons/fa6";
+import { FaMoon } from "react-icons/fa6";
 
 const Header = () => {
   return (
     <>
-      <Navbar>Header</Navbar>
+      <Navbar className="border-b-2 p-2">
+        <Link
+          to="/"
+          className="self-center font-semibold text-sm sm:text-xl dark:text-white"
+        >
+          <span className="px-2 py-1 rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+            Shyam
+          </span>
+          Blog
+        </Link>
+        {/* <form action="" className="flex items-center ">
+          <div className="relative">
+            <TextInput
+              id="text"
+              type="text"
+              placeholder="search................."
+              required
+              className="pl-10"
+            />
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <CiSearch className="text-gray-400" />
+            </div>
+          </div>
+        </form> */}
+
+         <div className="flex justify-center">
+          <Link to="/" className="mr-4 ">Home</Link>
+          <Link to="/about">About</Link>
+        </div> 
+
+        {/* <div className="border border-black-500 p-1 rounded-md px-4  hover:text-white text-white bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 ">Sigin</div>  */}
+
+        <div className="flex gap-2">
+          <Button className="w-12 h-10 hidden sm:inline rounded-md" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to="/sigin">
+          <Button color="gray" className="p-2 rounded-md bg-blue-500 font-semibold text-white">Sigin</Button> 
+          </Link>
+        </div>
+       
+      </Navbar>
     </>
   );
 };
+
 export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("flowbite/plugin")],
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
