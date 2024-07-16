12fd64f86696462f89f547e4431ffda50e3cd1ed
Shyam Kumar <imshyam2021@gmail.com>
2024-07-16 22:19:11 +0530

    shyam

--- client/src/pages/Sigin.jsx
+++ client/src/pages/Sigin.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sigin = () => {
-  return <div>Sigin</div>;
+  return (
+    <>
+      <div>sigin</div>
+    </>
+  );
 };
 
-export default Sigin
+export default Sigin;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,70 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <>
+      <div className="min-h-screen mt-20 ">
+        <div className="flex  mx-auto flex-col md:flex-row md:items-center gap-5 ml-10 mr-10">
+          {/* left */}
+          <div className="flex-1">
+            <p className="text-2xl font-semibold font-serif">
+              Shyam <span className="text-pink-500">Blog</span>
+            </p>
+            <p className="text-md">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla
+              consequuntur labore, tempora fuga provident sit aut obcaecati
+              excepturi voluptatum nam enim fugit repellat minima quidem nemo
+              consectetur aliquid eos ullam.
+            </p>
+          </div>
+          {/* Right */}
+          <div className="flex-1">
+            <form action="" className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="username">UserName</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="enter your username"
+                  className="rounded-md"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="enter your email"
+                  className="rounded-md"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="enter your password"
+                  className="rounded-md"
+                />
+              </div>
+              <button className="border p-2 bg-gradient-to-r  from-indigo-500 via-purple-500 to-pink-500 rounded-md text-xl text-white">
+                Submit
+              </button>
+            </form>
+            <div className="flex gap-2 mt-5">
+              <span className="">Have an Account</span>
+              <Link to="/sigin" className="text-blue-500">
+                Sigin
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Signup;
